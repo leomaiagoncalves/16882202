fde70c5327a934dc9ef4c5a8a26980816325760c
leomaiagoncalves <leomaia@usp.br>
2025-06-16 16:27:48 -0300

    Update aleatorio2.h

--- jogadores/aleatorio2.h
+++ jogadores/aleatorio2.h
@@ -2,6 +2,7 @@
 #define ALEATORIO2_H
 
 #include "../carta.h"
+#include "../mao.h"
 
 void iniciar_aleatorio2(int id, int total_jogadores);
 void nova_rodada_aleatorio2(int rodada, Carta carta_virada, int n_cartas, Carta* mao);
