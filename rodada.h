de586b85ac2a57b820c6589debd84d1879f9aecb
leomaiagoncalves <leomaia@usp.br>
2025-06-16 16:12:08 -0300

    Update rodada.h

--- rodada.h
+++ rodada.h
@@ -1,7 +1,7 @@
 #ifndef RODADA_H
 #define RODADA_H
 
-#include "carta.h"
+#include "cartas.h"
 
 typedef struct {
     int numero;
@@ -23,7 +23,6 @@ void iniciar_rodada(Rodada* r, int numero, int num_jogadores, int jogador_inicia
 void imprimir_maos(const Rodada* r);
 void atualizar_jogador_inicial(Rodada* r, int vencedor);
 
-// Novas funções adicionadas
 void embaralhar_e_distribuir_maos(Rodada* r, int num_jogadores, Carta* baralho);
 void processar_resultado_turno(Rodada* r, int vencedor);
 void atualizar_pontuacoes(Rodada* r, int* placar);
