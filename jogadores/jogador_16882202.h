29052f8b983effb55d86310318101bdce66364f4
leomaiagoncalves <leomaia@usp.br>
2025-06-16 03:09:18 -0300

    Update jogador_16882202.h 2

--- jogadores/jogador_16882202.h
+++ jogadores/jogador_16882202.h
@@ -3,10 +3,10 @@
 
 #include "../mao.h"
 
-void iniciar_16882202(int id, int total);
-void nova_rodada_16882202(int rodada, Carta carta_virada, int n_cartas, Carta* mao);
-int apostar_16882202(int* apostas);
-int jogar_16882202(Carta* mesa, int num_na_mesa, int* cartas_jogadas, int n_cartas_restantes);
-const char* nome_16882202(void);
+void iniciar_jogador_16882202(int id, int total);
+void nova_rodada_jogador_16882202(int rodada, Carta carta_virada, int n_cartas, Carta* mao);
+int apostar_jogador_16882202(int* apostas);
+int jogar_jogador_16882202(Carta* mesa, int num_na_mesa, int* cartas_jogadas, int n_cartas_restantes);
+const char* nome_jogador_16882202(void);
 
 #endif
