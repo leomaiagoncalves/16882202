1de0d1eb93b06ef21683bd2a1761ae72a59c8f48
leomaiagoncalves <leomaia@usp.br>
2025-06-16 16:44:58 -0300

    Update mao.h

--- mao.h
+++ mao.h
@@ -2,6 +2,7 @@
 #define MAO_H
 
 #include "carta.h"
+#include "rodada.h"
 
 int comparar_cartas(Carta a, Carta b, Valor manilha);
 int resolver_mao(Jogada* jogadas, int num_jogadores, Valor manilha);
