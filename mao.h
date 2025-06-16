8c93ad217397c64c09997024e93fc0fa6ca66cb5
leomaiagoncalves <leomaia@usp.br>
2025-06-16 16:39:29 -0300

    Update mao.h

--- mao.h
+++ mao.h
@@ -3,13 +3,7 @@
 
 #include "carta.h"
 
-typedef struct {
-    int jogador_id;
-    Carta carta;
-} Jogada;
-
 int comparar_cartas(Carta a, Carta b, Valor manilha);
 int resolver_mao(Jogada* jogadas, int num_jogadores, Valor manilha);
-// retorna o índice do vencedor ou -1 se houve empate entre não-manilhas
 
 #endif
